a9db1f19a7b4d3a214470b1acce548cd7a2df99a
Ashirvad Singh <145483361+ashirvad001@users.noreply.github.com>
2026-08-08 22:22:06 +0530

    Time: 25 ms (55.29%), Space: 51.6 MB (47.92%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+    Map<Integer,Integer> map =new HashMap<>();
+
+     map.put(0,1);
+
+    int prefixSum = 0;
+    int ans = 0;
+
+    for (int num : nums){
+     prefixSum+=num;
+
+    int rem=prefixSum %k;
+
+    if (rem < 0){
+        rem += k;
+    }
+
+    if(map.containsKey(rem)){
+        ans+=map.get(rem);
+    }
+    map.put(rem,map.getOrDefault(rem, 0)+1);
+}
+return ans;
+    }
+}
